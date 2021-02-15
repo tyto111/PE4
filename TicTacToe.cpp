35217efa71514a98bbf717c3b266d66619134122
tyto111 <tyto1511@colorado.edu>
2021-02-15 22:53:54 +0000

    created create board

---
+++ TicTacToe.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#define ROW 3
+#define COL 3
+
+void createBoard(std::string gameBoard[ROW][COL]){
+    for (int i = 0; i < ROW; i++){
+        for (int j = 0; j < COL; j++){
+            gameBoard[i][j] = "";
+        }
+    }
+}
+
+
+int main(){
+    std::string board[ROW][COL];
+    createBoard(board);
+    
+    return 0;
+}
