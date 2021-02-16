494093b40a27c1c8c2a8c2548b2e0915f38e80e3
tyto111 <tyto1511@colorado.edu>
2021-02-16 02:31:18 +0000

    Player choice working correctly

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -20,10 +20,20 @@ void displayBoard(std::string gameBoard[ROW][COL]){
     }
 }
 
+void getPlayerChoice(int arr[]){
+    int row, col;
+    std::cout << "Please enter row: ";
+    std::cin >> row;
+    std::cout<< "Please enter col: ";
+    std::cin >> col;
+    arr[0] = row;
+    arr[1] = col;
+}
+
 
 int main(){
+    int choice[2];
     std::string board[ROW][COL];
     createBoard(board);
-    
     return 0;
 }
